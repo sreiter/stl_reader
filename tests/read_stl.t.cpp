ae8810064147e4d09f0c27825b8c24a52cacf134
Sebastian Reiter <s.b.reiter@gmail.com>
2023-02-26 08:45:44 +0100

    Add test which reads a binary stl file.

--- tests/read_stl.t.cpp
+++ tests/read_stl.t.cpp
@@ -4,7 +4,7 @@
 TEST (readSTL, asciiSphere)
 {
   stl_reader::StlMesh<> mesh;
-  mesh.read_file ("data/sphere.stl");
+  mesh.read_file ("data/ascii_sphere.stl");
   
   EXPECT_EQ (mesh.num_vrts (), 12);
   EXPECT_EQ (mesh.num_tris (), 20);
@@ -13,4 +13,16 @@ TEST (readSTL, asciiSphere)
   EXPECT_EQ (mesh.solid_tris_begin (0), 0);
   EXPECT_EQ (mesh.solid_tris_begin (1), 2);
   EXPECT_EQ (mesh.solid_tris_end (1), 20);
+}
+
+TEST (readSTL, binarySphere)
+{
+  stl_reader::StlMesh<> mesh;
+  mesh.read_file ("data/binary_sphere.stl");
+  
+  EXPECT_EQ (mesh.num_vrts (), 12);
+  EXPECT_EQ (mesh.num_tris (), 20);
+  EXPECT_EQ (mesh.num_solids (), 1);
+  EXPECT_EQ (mesh.solid_tris_begin (0), 0);
+  EXPECT_EQ (mesh.solid_tris_end (0), 20);
 }
